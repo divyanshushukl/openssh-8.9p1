86ffbc7e41ae74bd3058ac8d5b0285d1b8f10837
divyanshushukl <101639464+divyanshushukl@users.noreply.github.com>
2022-04-30 03:00:28 +0530

    Update ssh-binary.sh

--- ssh-binary.sh
+++ ssh-binary.sh
@@ -2,10 +2,12 @@ sudo apt install build-essential zlib1g-dev libssl-dev libpam0g-dev libselinux1-
 sudo mkdir /var/lib/sshd
 sudo chmod -R 700 /var/lib/sshd/
 sudo chown -R root:sys /var/lib/sshd/
-dirName="/tmp/openssh-8.9p1"
-cd -- "$dirName"
+
 wget --no-cache -c https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.9p1.tar.gz
 tar -xvzf openssh-8.9p1.tar.gz -C /tmp/
+
+dirName="/tmp/openssh-8.9p1"
+cd -- "$dirName"
 sleep 10
 
 ./openssh-8.9p1/configure --with-kerberos5 --with-md5-passwords --with-pam --with-selinux --with-privsep-path=/var/lib/sshd/ --sysconfdir=/etc/ssh
