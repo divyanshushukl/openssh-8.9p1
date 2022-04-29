0c97a32e5b357d8224407b5695e083bb92619b27
divyanshushukl <101639464+divyanshushukl@users.noreply.github.com>
2022-04-30 01:44:30 +0530

    Add files via upload

---
+++ ssh-binary.sh
@@ -0,0 +1,11 @@
+#update outdated ssh version
+sudo apt install build-essential zlib1g-dev libssl-dev libpam0g-dev libselinux1-dev libkrb5-dev -y
+sudo mkdir /var/lib/sshd
+sudo chmod -R 700 /var/lib/sshd/
+sudo chown -R root:sys /var/lib/sshd/
+wget -c https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-8.9p1.tar.gz -O /tmp/openssh-8.9p1.tar.gz
+tar -xzf /tmp/openssh-8.9p1.tar.gz
+cd /tmp/openssh-8.9p1/
+./configure --with-kerberos5 --with-md5-passwords --with-pam --with-selinux --with-privsep-path=/var/lib/sshd/ --sysconfdir=/etc/ssh
+make
+make install
